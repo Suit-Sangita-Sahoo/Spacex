afc83ac77e926bf05ae87bd478d62930772ee1c5
Suit-Sangita-Sahoo <suitsangitasahoo@gmail.com>
2025-11-14 21:21:08 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css"
+const Footer=({type})=>{
+    return(
+        <div id="footer">
+            <div id="foot">
+                <ul>
+                    <li>SPACEX2024</li>
+                    <li>PRIVACY POLICY</li>
+                    <li>SUPPLIERS</li>
+                </ul>
+            </div>
+        
+        </div>
+    )
+}
+export default Footer
